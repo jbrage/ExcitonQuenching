1b32358ce7143926aa9f1d112bf05407e499b509
jbrage <jeppebrage@gmail.com>
2018-05-01 09:41:53 +0200

    updated setup

--- cython/setup.py
+++ cython/setup.py
@@ -1,6 +1,10 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
 from Cython.Build import cythonize
+import numpy
 
 setup(
-    ext_modules=cythonize("evolveDensitiesCython.pyx"),
+    ext_modules=[
+        Extension("evolveDensitiesCython", ["evolveDensitiesCython.c"],
+                  include_dirs=[numpy.get_include()]),
+    ],
 )
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
 import os, sys
 sys.path.append('./cython')
